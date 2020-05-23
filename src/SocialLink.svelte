b9f4913b33bd3adf7e808e8d6f19a9063f0a90b3
Julian Burgess <aubergene@gmail.com>
2020-05-23 12:05:40 +0100

    social links

---
+++ src/SocialLink.svelte
@@ -0,0 +1,37 @@
+<script>
+  export let kind;
+  export let value;
+
+  let icon;
+  let url;
+  let text;
+
+  switch (kind) {
+    case "website":
+      icon = "";
+      url = value;
+      text = value;
+      break;
+    case "instagram":
+      icon = `<span class="icon"><i class="fab fa-instagram"></i></span>`;
+      url = `https://instagram.com/${value}`;
+      text = `@${value}`;
+      break;
+    case "twitter":
+      icon = `<span class="icon"><i class="fab fa-twitter"></i></span>`;
+      url = `https://twitter.com/${value}`;
+      text = `@${value}`;
+      break;
+
+    default:
+      throw new Error(`Unknown social kind ${kind}`);
+      break;
+  }
+</script>
+
+{#if value}
+  <div class="social-profile">
+    {@html icon}
+    <a href={url} target="_blank">{value}</a>
+  </div>
+{/if}
--- src/WorkCard.svelte
+++ src/WorkCard.svelte
@@ -1,14 +1,14 @@
 <script>
+  import SocialLink from "./SocialLink.svelte";
+
   export let work;
+
+  const placeholderUrl = "https://via.placeholder.com/1000x500";
 </script>
 
 <style>
   .card {
     margin-top: 2em;
-  }
-
-  .social-profile {
-    white-space: nowrap;
   }
 
   time {
@@ -23,29 +23,36 @@
 <div class="card" name={work.slug}>
   <div class="card-image">
     <figure class="image is-2by1">
-      <img src={work.thumbUrl} alt={work.title} loading="lazy" />
+      <img
+        src={work.thumbUrl || placeholderUrl}
+        alt={work.title}
+        loading="lazy" />
     </figure>
   </div>
   <div class="card-content">
     <div class="content">
-      <p class="title is-4">{work.title}</p>
-      <a href="#schedule">
-        {#if work.track}
-          <div>
-            Track:
-            <span class="track {work.track.slug}-bg">{work.track.name}</span>
-          </div>
-        {/if}
-        <div>
-          Start time:
-          <time datetime="2016-1-1">{work.date}</time>
-        </div>
-      </a>
-      <div>{work.desc || 'No description provided'}</div>
+      <h4 class="title is-4">{work.title}</h4>
+
       <div>
-        <a href="#">#keywords</a>
-        <a href="#">#themes</a>
+        <span class="tag">{work.form}</span>
       </div>
+
+      <ul>
+        {#each work.performances as p}
+          <li>
+            <div>
+              Track:
+              <span class="track {p.track.slug}-bg">{p.track.name}</span>
+            </div>
+            <div>
+              Start time:
+              <time datetime={p.startTime.toISOString()}>{p.startTime}</time>
+            </div>
+          </li>
+        {/each}
+      </ul>
+
+      <div>{work.desc || 'No description provided'}</div>
     </div>
 
     <div class="media">
@@ -59,18 +66,9 @@
       </div>
       <div class="media-content">
         <p class="title is-5">{work.artist}</p>
-        <div class="social-profile">
-          IG:
-          <a href="https://instagram.com/johnsmith">@{work.instagram}</a>
-        </div>
-        <div class="social-profile">
-          TW:
-          <a href="https://twitter.com/johnsmith">@{work.twitter}</a>
-        </div>
-        <div class="social-profile">
-          www:
-          <a href="https://www.johnsmith.com">{work.website}</a>
-        </div>
+        <SocialLink kind="instagram" value={work.instagram} />
+        <SocialLink kind="twitter" value={work.twitter} />
+        <SocialLink kind="website" value={work.website} />
       </div>
     </div>
 

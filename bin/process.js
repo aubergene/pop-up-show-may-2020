c6cbde2f9545c8e48fa58e3ca943065bfc9b65b3
Julian Burgess <aubergene@gmail.com>
2020-05-23 13:51:47 +0100

    Fetch imgur albums

--- bin/process.js
+++ bin/process.js
@@ -5,131 +5,145 @@ import slugify from "slugify";
 import { pick } from "lodash";
 import { allTracks } from "../src/config";
 import { slugify2 } from "../src/helpers";
+import fetch from "node-fetch";
+import PQueue from "p-queue";
 
-const projects = [];
-
-// being pretty strict about what chars I allow in slugs
-const BAD_SLUG_CHARS = /[*+~,.()'"!:@?/#_\[\]]/g;
+const imgurClientID = "cf9c0dd51e1126b";
+const BAD_SLUG_CHARS = /[*+~,.()'"!:@?/#_\[\]]/g; // being pretty strict about what chars I allow in slugs
 const toSlug = (str) => slugify(str, { lower: true, remove: BAD_SLUG_CHARS });
-
-const allTrackNames = allTracks.map((d) => d.name);
-// console.log(allTrackNames);
 
 const rawDir = `${__dirname}/../raw/`;
 const scheduleFiles = [
   "Pop up show timetable - May 2020 - 2020-05-29.csv",
   "Pop up show timetable - May 2020 - 2020-05-30.csv",
 ];
-
 const worksFile = `${rawDir}Pop-up exhibition, final form (Responses) - Form responses 1.csv`;
-const works = csvParseRows(fs.readFileSync(worksFile, "utf-8"), (d, i) => {
-  // The column headers (row 0) are garbage so do it manually
-  if (i === 0) return null;
-  return {
-    row: i + 1,
-    artist: d[1],
-    officialName: d[2],
-    title: d[3],
-    desc: d[4],
-    thumbUrl: d[5],
-    url: d[6],
-    website: d[7],
-    instagram: d[8],
-    twitter: d[9],
-    facebook: d[10],
-  };
-});
 
-// console.log(works);
-
+const queue = new PQueue({ concurrency: 6 });
+const projects = [];
+const allTrackNames = allTracks.map((d) => d.name);
+let works;
 const worksByTitle = new Map();
 
-works.forEach((row) => {
-  Object.keys(row).forEach((k) => {
-    if (typeof row[k] === "string") {
-      row[k] = row[k].trim();
-    }
+// console.log(allTrackNames);
+
+loadWorks().then(loadSchedules).then(output);
+
+async function loadWorks() {
+  works = csvParseRows(fs.readFileSync(worksFile, "utf-8"), (d, i) => {
+    // The column headers (row 0) are garbage so do it manually
+    if (i === 0) return null;
+    return {
+      row: i + 1,
+      artist: d[1],
+      officialName: d[2],
+      title: d[3],
+      desc: d[4],
+      thumbUrl: d[5],
+      url: d[6],
+      website: d[7],
+      instagram: d[8],
+      twitter: d[9],
+      facebook: d[10],
+    };
   });
 
-  const workSlug = toSlug(row.title);
+  // console.log(works);
 
-  row.instagram = row.instagram
-    .replace(/https:\/\/(www.)?instagram.com\//, "")
-    .replace(/\/$/, "")
-    .replace("@", "");
+  await queue.addAll(
+    works.map((row) => async () => {
+      Object.keys(row).forEach((k) => {
+        if (typeof row[k] === "string") {
+          row[k] = row[k].trim();
+        }
+      });
 
-  row.twitter = row.twitter
-    .replace(/https:\/\/(www.)?twitter.com\//, "")
-    .replace(/\/$/, "")
-    .replace("@", "");
+      const workSlug = toSlug(row.title);
 
-  worksByTitle.set(workSlug, row);
-});
+      row.instagram = row.instagram
+        .replace(/https:\/\/(www.)?instagram.com\//, "")
+        .replace(/\/$/, "")
+        .replace("@", "");
+
+      row.twitter = row.twitter
+        .replace(/https:\/\/(www.)?twitter.com\//, "")
+        .replace(/\/$/, "")
+        .replace("@", "");
+
+      row.imgur = await getImgur(row);
+
+      worksByTitle.set(workSlug, row);
+    })
+  );
+}
 
 // console.log(worksByTitle);
 
-scheduleFiles.forEach((file, day) => {
-  const filePath = path.resolve(rawDir + file);
-  const rows = csvParse(fs.readFileSync(filePath, "utf-8"), autoType);
+async function loadSchedules() {
+  scheduleFiles.forEach((file, day) => {
+    const filePath = path.resolve(rawDir + file);
+    const rows = csvParse(fs.readFileSync(filePath, "utf-8"), autoType);
 
-  rows.forEach((row, i) => {
-    row.row = i + 1;
+    rows.forEach((row, i) => {
+      row.row = i + 1;
 
-    if (!row.title) return;
+      if (!row.title) return;
 
-    row.trackId = allTrackNames.indexOf(row.track);
-    if (row.trackId < 0) {
-      console.warn(`Bad track "${row.track}" row ${i + 1} in ${file}`);
-    }
-
-    Object.keys(row).forEach((k) => {
-      if (typeof row[k] === "string") {
-        row[k] = row[k].trim();
+      row.trackId = allTrackNames.indexOf(row.track);
+      if (row.trackId < 0) {
+        console.warn(`Bad track "${row.track}" row ${i + 1} in ${file}`);
       }
+
+      Object.keys(row).forEach((k) => {
+        if (typeof row[k] === "string") {
+          row[k] = row[k].trim();
+        }
+      });
+
+      row.day = day + 1;
+      row.slug = toSlug(row.title);
+
+      const [hour, min] = row.startTime.split(":");
+      row.startUTC = [hour - 1, min].join(":"); // quick hack
+
+      if (worksByTitle.has(row.slug)) {
+        const work = worksByTitle.get(row.slug);
+        work.used = true;
+        row.matched = true;
+        Object.assign(row, work);
+      }
+
+      projects.push(row);
     });
-
-    row.day = day + 1;
-    row.slug = toSlug(row.title);
-
-    const [hour, min] = row.startTime.split(":");
-    row.startUTC = [hour - 1, min].join(":"); // quick hack
-
-    if (worksByTitle.has(row.slug)) {
-      const work = worksByTitle.get(row.slug);
-      work.used = true;
-      row.matched = true;
-      Object.assign(row, work);
-    }
-
-    projects.push(row);
   });
-});
+}
 
-const unmatchedProjects = projects.filter((d) => !d.matched);
-console.log("Projects which don't have work");
-console.log(
-  unmatchedProjects
-    .map((d) => {
-      return `Day ${d.day}\tRow ${d.row}\t${d.slug}`;
-    })
-    .join("\n")
-);
-console.log("Total", unmatchedProjects.length);
-console.log();
-console.log();
+function output() {
+  const unmatchedProjects = projects.filter((d) => !d.matched);
+  console.log("Projects which don't have work");
+  console.log(
+    unmatchedProjects
+      .map((d) => {
+        return `Day ${d.day}\tRow ${d.row}\t${d.slug}`;
+      })
+      .join("\n")
+  );
+  console.log("Total", unmatchedProjects.length);
+  console.log();
+  console.log();
 
-const unusedWorks = works.filter((d) => !d.used);
-console.log("Works which don't match a scheduled event");
-console.log(
-  unusedWorks
-    .map((d) => {
-      return `Row ${d.row}\t${d.title} - ${d.officialName}`;
-    })
-    .join("\n")
-);
-console.log("Total", unusedWorks.length);
+  const unusedWorks = works.filter((d) => !d.used);
+  console.log("Works which don't match a scheduled event");
+  console.log(
+    unusedWorks
+      .map((d) => {
+        return `Row ${d.row}\t${d.title} - ${d.officialName}`;
+      })
+      .join("\n")
+  );
+  console.log("Total", unusedWorks.length);
 
-const headers = `
+  const headers = `
     day
     trackId
     startUTC
@@ -139,6 +153,7 @@ const headers = `
     form
     desc
     thumbUrl
+    imgur
     url
     website
     instagram
@@ -146,23 +161,23 @@ const headers = `
     facebook
     module
 `
-  .trim()
-  .split("\n")
-  .map((d) => d.trim());
+    .trim()
+    .split("\n")
+    .map((d) => d.trim());
 
-// console.log(headers);
+  // console.log(headers);
 
-const out = projects.map((d) => pick(d, headers));
+  const out = projects.map((d) => pick(d, headers));
 
-const publicDir = path.resolve(`${__dirname}/../public/`);
-const projectsPath = `${publicDir}/data/works.csv`;
-fs.mkdirSync(path.dirname(projectsPath), { recursive: true });
-fs.writeFileSync(projectsPath, csvFormat(out));
+  const publicDir = path.resolve(`${__dirname}/../public/`);
+  const projectsPath = `${publicDir}/data/works.csv`;
+  fs.mkdirSync(path.dirname(projectsPath), { recursive: true });
+  fs.writeFileSync(projectsPath, csvFormat(out));
 
-const cssColors = allTracks
-  .map((track) => {
-    const slug = slugify2(track.name);
-    return `
+  const cssColors = allTracks
+    .map((track) => {
+      const slug = slugify2(track.name);
+      return `
 .${slug}-bg {
   background-color: ${track.color};
 }
@@ -170,8 +185,42 @@ const cssColors = allTracks
   border-color: ${track.color};
 }
   `.trim();
-  })
-  .join("\n");
+    })
+    .join("\n");
 
-const cssColorsPath = `${publicDir}/colors.css`;
-fs.writeFileSync(cssColorsPath, cssColors);
+  const cssColorsPath = `${publicDir}/colors.css`;
+  fs.writeFileSync(cssColorsPath, cssColors);
+}
+
+var requestOptions = {
+  method: "GET",
+  headers: {
+    Authorization: `Client-ID ${imgurClientID}`,
+  },
+};
+
+async function getImgur(work) {
+  const url = work.thumbUrl.trim();
+
+  if (!url) return null;
+
+  if (/\/a\//.exec(url)) {
+    return getAlbumLink(url).catch((error) => {
+      console.error(`Error fetching ${url} - row ${work.row}`);
+      console.error(error);
+    });
+  }
+
+  return path.basename(url).replace(/\..*/, "");
+}
+
+function getAlbumLink(albumUrl) {
+  const albumHash = path.basename(albumUrl);
+  console.log(`fetching ${albumHash} — ${albumUrl}`);
+  return fetch(
+    `https://api.imgur.com/3/album/${albumHash}/images`,
+    requestOptions
+  )
+    .then((response) => response.json())
+    .then((result) => path.basename(result.data[0].link).replace(/\..*/, ""));
+}

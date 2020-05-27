fe592dff55c168b50d43eb6a208da7c8fc51d2c1
Julian Burgess <aubergene@gmail.com>
2020-05-27 17:40:39 +0100

    Add social links to intro

--- src/Intro.svelte
+++ src/Intro.svelte
@@ -7,7 +7,18 @@
   <p>
     For the very first time all performances, artwork, and games will be
     showcased entirely online via
-    <a href="https://zoom.us/" target="_blank">Zoom</a>
-    .
+    <a href="https://zoom.us/" target="_blank">Zoom</a>.
+  </p>
+
+  <p>
+    You can RSVP to our
+    <a href="https://www.facebook.com/events/1942782225857241/" target="_blank">
+      Facebook event</a>, follow us on
+    <a href="https://www.instagram.com/goldsmithscomparts/" target="_blank">
+      Instagram @goldsmithscomparts
+    </a>
+    and
+    <a href="https://twitter.com/goldcomparts/" target="_blank">
+      Twitter @goldcomparts</a>.
   </p>
 </div>
